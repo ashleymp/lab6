6439ab60958fd5d424c81c0547a50ebd87d71f02
Ashley Perez <amp016@ucsd.edu>
2015-02-13 21:41:45 +0000

    Finished lab6

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -27,6 +27,9 @@ function addProjectDetails(e) {
 	var idNumber = projectID.substr('project'.length);
 
 	console.log("User clicked on project " + idNumber);
+	var url = "/project/" + idNumber; 
+	console.log("URL: " + url); 
+	$.get(url, projectCallback);
 }
 
 /*
@@ -35,4 +38,105 @@ function addProjectDetails(e) {
  */
 function randomizeColors(e) {
 	console.log("User clicked on color button");
+
+	var url = "/palette";
+	$.get(url, ranColorsCallback);
 }
+
+function ranColorsCallback(result) {
+	console.log(result);
+	var clr = result['colors'];
+	var colors = clr['hex'];
+	console.log(colors);
+
+	$('body').css('background-color', colors[0]);
+	$('.thumbnail').css('background-color', colors[1]);
+	$('h1, h2, h3, h4, h5').css('color', colors[2]);
+	$('p').css('color', colors[3]);
+	$('.project img').css('opacity', .75); 
+}
+
+
+
+function projectCallback (result) {
+	console.log(result);
+	var project = '<a href="#" class="thumbnail">' +
+		'<img src="' + result['image'] + '"class="detailsImage">' +
+		'<p>' + result['title'] + '</p>' +
+		'<p>' + result['summary'] + '</p></a>'; 
+
+	S("#project" + result['id']).find(".details").html(project);
+	console.log("completed"); 
+
+
+
+}
+/*
+'use strict';
+
+// Call this function when the page loads (the "ready" event)
+$(document).ready(function() {
+	initializePage();
+})
+
+/*
+ * Function that is called when the document is ready.
+ *
+function initializePage() {
+	$('.project a').click(addProjectDetails);
+
+	$('#colorBtn').click(randomizeColors);
+}
+
+/*
+ * Make an AJAX call to retrieve project details and add it in
+ *
+function addProjectDetails(e) {
+	// Prevent following the link
+	e.preventDefault();
+
+	// Get the div ID, e.g., "project3"
+	var projectID = $(this).closest('.project').attr('id');
+	// get rid of 'project' from the front of the id 'project3'
+	var idNumber = projectID.substr('project'.length);
+
+	console.log("User clicked on project " + idNumber);
+
+	$.get("/projects/"+idNumber, addProject);
+	console.log("/projects/"+idNumber);
+	
+}
+
+function addProject(result) {
+	console.log(result);
+
+	var project = '<a href="#" class="thumbnail">' +
+		'<img src="' + result['image'] + '"class="detailsImage">' +
+		'<p>' + result['title'] + '</p>' +
+		'<p>' + result['summary'] + '</p></a>'; 
+
+	S("#project" + result['id']).find(".details").html(project);
+	console.log("completed");
+}
+
+/*
+ * Make an AJAX call to retrieve a color palette for the site
+ * and apply it
+ *
+function randomizeColors(e) {
+	console.log("User clicked on color button");
+	$.get("/paletter", randomColors);
+}
+
+function randomColors(e) {
+	console.log(result);
+	var clr = result['colors'];
+	var colors = clr['hex'];
+	console.log(colors);
+
+	$('body').css('background-color', colors[0]);
+	$('.thumbnail').css('background-color', colors[1]);
+	$('h1, h2, h3, h4, h5').css('color', colors[2]);
+	$('p').css('color', colors[3]);
+	$('.project img').css('opacity', .75); 
+}*/
